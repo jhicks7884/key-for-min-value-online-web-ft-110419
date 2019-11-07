98789a012bd0ba211388d365d908aab78c65ab04
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 19:25:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
 min_value = nil
 min_key = nil
-name_hash.each do |k, v|
+name_hash.each do |key, value|
   if min_value == nil
-    min_value = v
-    min_key = k
+    min_value = value
+    min_key = key
   end
 end
 min_key
